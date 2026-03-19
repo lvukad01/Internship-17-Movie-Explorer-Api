b264b9bfdcffd15b4cee75964f95882e39a6dfa4
lvukad01 <lana.vukadin1234@gmail.com>
2026-03-19 15:57:50 +0100

    Implement findAll in favorites module

--- src/favorites/favorites.controller.ts
+++ src/favorites/favorites.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Get, UseGuards, Req } from '@nestjs/common';
 import { FavoritesService } from './favorites.service';
+import { ApiOperation } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('favorites')
 export class FavoritesController {
   constructor(private readonly favoritesService: FavoritesService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  @ApiOperation({summary:'Get favorites'})
+  findAll(@Req() req){
+    const userId=req.user.userId
+    return this.favoritesService.findAll(userId);
+  }
 }
--- src/favorites/favorites.service.ts
+++ src/favorites/favorites.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class FavoritesService {}
+export class FavoritesService {
+    constructor(private prisma: PrismaService) {}
+
+    async findAll(userId: number){
+        return this.prisma.favorite.findMany({
+            where:{userId},
+            include:{movie:true}
+        })
+    }
+}
